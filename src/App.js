cd93cc357c0bc73f70a5a6575e81c0e3b4fc611d
DESKTOP-E56JJOD\batac <batacbryankit@gmail.com>
2022-12-30 08:59:07 +0800

    Globalize the task data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Header from "./components/Header";
 import Tasks from './components/Tasks';
+import { useState } from "react";
 
-function App() {
-	const name = '';
+const App = () => {
+	const tasksData = [
+		{
+		  id: 1,
+		  text: 'Doctors Appointment',
+		  day: 'Dec 01 2022',
+		  reminder: true,
+		},
+		{
+		  id: 2,
+		  text: 'Doctors Appointment 2',
+		  day: 'Dec 02 2022',
+		  reminder: false,
+		},
+	];
+	const [tasks, setTasks] = useState(tasksData)
 	return (
 		<div className="container">
 			<Header title='Task Tracker' />
-			<Tasks />
+			<Tasks tasks={tasks}/>
 		</div>
 	);
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,23 +1,9 @@
-const tasks = [
-	{
-	  id: 1,
-	  text: 'Doctors Appointment',
-	  day: 'Dec 01 2022',
-	  reminder: true,
-	},
-	{
-	  id: 2,
-	  text: 'Doctors Appointment 2',
-	  day: 'Dec 02 2022',
-	  reminder: false,
-	},
-];
-
-const Tasks = () => {
+const Tasks = ({tasks}) => {
+	
 	return (
 		<div>
 			{tasks.map((task) => (
-				<h3>{task.text}</h3>
+				<h3 key={task.id}>{task.text}</h3>
 			))}
 		</div>
 	)
